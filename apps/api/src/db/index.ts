95aab5f7074255cdff390677a30e0c601bbf5f88
José Pinheiro <josepsousa2012@gmail.com>
2025-05-27 17:18:44 -0300

    feat: update package.json and pnpm-lock.yaml to remove unused dependencies; refactor app and database connection setup for improved structure and clarity

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,6 +1,24 @@
 import { DataSource } from "typeorm";
 import { env } from "../shared/env";
 import * as path from 'path';
+import { FastifyInstance } from 'fastify';
+import { Transaction } from "./entities/transaction.entity";
+import { Session } from "inspector";
+import { Category } from "./entities/category.entity.";
+import { User } from "./entities/user.entity";
+
+// export async function connectToDatabase(app: FastifyInstance) {
+//     app.register(dbConnection, {
+//         type: 'postgres',
+//         host: env.PGHOST,
+//         username: env.PGUSER,
+//         password: env.PGPASSWORD,
+//         database: env.PGDATABASE,
+//         port: 5432,
+//         entities: [path.join(__dirname, '../entities/*.entity{.ts,.js}')],
+//         synchronize: true,
+//     })
+// }
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
@@ -9,6 +27,12 @@ export const AppDataSource = new DataSource({
     password: env.PGPASSWORD,
     database: env.PGDATABASE,
     port: 5432,
-    entities: [path.join(__dirname, '../entities/*.entity{.ts,.js}')],
+    // entities: [path.resolve(__dirname, '../entities/**/*.entity.{ts,js}')],
+    entities: [
+        Transaction,
+        User,
+        Category,
+        Session
+    ],
     synchronize: true,
 })

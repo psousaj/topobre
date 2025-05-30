29bf5f2c381cd3a18ab4a95ef0964b7715b34f7c
José Pinheiro <josepsousa2012@gmail.com>
2025-05-29 22:31:56 -0300

    feat(transaction): update transaction schema and routes to use new fields and status
    - Changed transactionValue to valueInCents in transaction schema and routes.
    - Updated transactionType to type in transaction schema and routes.
    - Added user existence check in transaction creation route.
    - Set default transaction status to PENDING when creating a new transaction.
    - Modified error handling to replace backslashes in error messages.

--- apps/api/src/modules/transaction/transaction.schema.ts
+++ apps/api/src/modules/transaction/transaction.schema.ts
@@ -1,18 +1,16 @@
 import z from "zod";
-import { categorySchema } from "../category/category.schema";
 import { TransactionType } from "../../types";
 
 export const transactionSchema = z.object({
     id: z.string().uuid(),
-    transactionValue: z.number().positive('Valor deve ser positivo'),
+    valueInCents: z.number().positive('Valor deve ser positivo'),
     dueDate: z.string().refine(date => new Date(date) < new Date(), {
         message: 'Vencimento deve ser uma data futura',
     }).or(z.date()),
     description: z.string().min(3, 'Descrição deve ter ao menos 3 caracteres'),
-    transactionType: z.nativeEnum(TransactionType, {
-        errorMap: () => ({ message: 'Tipo deve ser payment ou receipt' }),
+    type: z.nativeEnum(TransactionType, {
+        errorMap: () => ({ message: 'Tipo da transação deve ser [\'INCOME\', \'EXPENSE\', \'TRANSFER\', \'PAYMENT\', \'RECEIPT\']' }),
     }),
     userId: z.string().optional(),
-    categoryId: z.string().cuid(),
-    category: categorySchema.optional(),
+    categoryId: z.string().uuid(),
 })

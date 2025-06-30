611c351fc798553377ed5d6281985c33352ccce7
José Pinheiro <josepsousa2012@gmail.com>
2025-06-30 00:46:42 -0300

    feat: remover pacotes supabase e arquivos relacionados

--- packages/supabase/index.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { env } from "@topobre/env"
-import { Database } from './types';
-
-export * from './types';
-
-export const supabaseClient = createClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
-export const supabaseServer = createClient<Database>(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY)

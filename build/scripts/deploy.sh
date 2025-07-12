1e02b92428e8075ab64b8d369d757e101a374bcb
José Pinheiro <josepsousa2012@gmail.com>
2025-07-12 11:34:35 -0300

    feat: Adiciona logs de debug no deploy
    Adiciona logs para imprimir as variáveis de ambiente durante o processo de deploy para auxiliar na depuração.

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -30,6 +30,19 @@ cat $ENV_FILE
 echo "🏷️ Usando tag da imagem: ${IMAGE_TAG}"
 echo "🚀 Subindo containers com docker compose..."
 
+echo "🐛 DEBUG: Testando leitura das variáveis do ambiente"
+echo "PGHOST=$PGHOST"
+echo "PGDATABASE=$PGDATABASE"
+echo "PGUSER=$PGUSER"
+echo "PGPASSWORD=$PGPASSWORD"
+echo "RESEND_API_KEY=$RESEND_API_KEY"
+echo "JWT_SECRET=$JWT_SECRET"
+echo "COOKIE_SECRET=$COOKIE_SECRET"
+echo "OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"
+echo "NEXT_PUBLIC_API_URL=$NEXT_PUBLIC_API_URL"
+echo "IMAGE_TAG=$IMAGE_TAG"
+echo "NODE_ENV=$NODE_ENV"
+
 docker compose --env-file $ENV_FILE -f docker-compose.apps.yml pull
 docker compose --env-file $ENV_FILE -f docker-compose.apps.yml down -v --remove-orphans
 docker compose --env-file $ENV_FILE -f docker-compose.apps.yml up -d

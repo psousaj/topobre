8734375b3fcaab495f87cbd7a942c0409ad4ef88
José Pinheiro <josepsousa2012@gmail.com>
2025-07-22 01:31:47 -0300

    chore(infra): Simplifica comando docker-compose down
    - Remove flags desnecessárias do comando `docker-compose down`
    - O objetivo é simplificar o script e evitar erros inesperados

--- build/scripts/infra.sh
+++ build/scripts/infra.sh
@@ -8,7 +8,7 @@ export NODE_ENV=production
 echo "🚀 Subindo containers de infraestrutura com docker compose..."
 
 docker compose -f docker-compose.infra.yml pull
-docker compose -f docker-compose.infra.yml down -v --remove-orphans
+docker compose -f docker-compose.infra.yml down 
 docker compose -f docker-compose.infra.yml up -d
 
 echo "✅ Deploy da infraestrutura finalizado com sucesso!"
